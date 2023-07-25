0d5dd91d66bd8958ab6c43f63c6caadd47b79cfd
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-25 08:11:45 +0300

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,7 @@
-def print_addition_result():
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
     from add_0 import add
 
     a = 1
     b = 2
-    result = add(a, b)
-
-    print(f"{a} + {b} = {result}")
-
-print_addition_result()
+    print("() + {} = {}".format(a, b, add(a, b)))
