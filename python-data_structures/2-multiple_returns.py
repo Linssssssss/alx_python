2423ae27631df2ef5d702a1c9d11e6809f4ac556
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-26 15:45:28 +0300

    More returns

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -2,12 +2,3 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_char = None if length == 0 else sentence[0]
     return length, first_char
-
-# Test the function
-sentence1 = "Hello, world!"
-result1 = multiple_returns(sentence1)
-print(result1) # Output: (13, 'H')
-
-sentence2 = ""
-result2 = multiple_returns(sentence2)
-print(result2) # Output: (0, None)
