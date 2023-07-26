d318d4cc95bb142cd0dda8b865cedcb5c22c8149
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-26 15:19:18 +0300

    Lists of lists = matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -9,11 +9,3 @@ def print_matrix_integer(matrix=[[]]):
             if j < num_cols - 1:
                 print(" ", end="")
                 print()
-# Test the function
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
