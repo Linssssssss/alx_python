527af9c3c1d56daab0f4d1f0698a8fb33245045c
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-26 19:12:04 +0300

    Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,16 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
-    # Get the dimensions of the matrix
-    rows = len(matrix)
-    if rows == 0:
-        return
-
-    columns = len(matrix[0])
-
-    # Calculate the maximum width of each element for formatting purposes
-    max_width = max(len(str(matrix[i][j])) for i in range(rows) for j in range(columns))
-
-    # Print the matrix
     for row in matrix:
-        for i in range(columns):
-            print("{:>{width}}".format(row[i], width=max_width), end=" ")
+        for num in row:
+            print("{:d}".format(num), end=' ')
         print()
