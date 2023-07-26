ee57aca56e22544ab4a2e9fa9b6ffbf8829e0487
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-26 15:52:36 +0300

    Lists of lists

--- python-data_structures/main_0.py
+++ python-data_structures/main_0.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-multiple_returns = __import__('2-multiple_returns').multiple_returns
+print_matrix_integer = __import__('1-print_matrix_integer').print_matrix_integer
 
-sentence = "Holberton"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print_matrix_integer(matrix)
