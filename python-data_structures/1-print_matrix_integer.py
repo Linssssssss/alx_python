5f359fd0e2ab87acf57e780817aa8928880771ff
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-26 15:50:21 +0300

    Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,9 @@
 def print_matrix_integer(matrix=[[]]):
     # Get the number of rows and columns in the matrix
     for row in matrix:
-        for col in row:
+        for col_idx, col in enumerate(row):
             # Print each integer in the matrix with proper formatting
-            print("{:d}".format(col), end=" " if col != row[-1] else "")
+            print("{:d}".format(col), end=" ")
+            if col_idx < len(row) - 1:
+                print(" ", end="")
             print() #Move to the next line after each row
