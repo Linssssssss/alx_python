2a01b124818123543c27f4cc00cfee0ba70fdc93
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-31 15:50:12 +0300

    Raise exception

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,8 +1,2 @@
 def raise_exception():
-    try:
-        value = "Hello" + 5
-    except TypeError:
-        print("Type error occured!")
-        raise 
-
-raise_exception()
+    raise TypeError("Exception raised")
