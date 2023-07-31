7a5dbc48623678e917242c10f05ed437e45fad50
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-31 15:52:16 +0300

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,12 +5,4 @@ def safe_print_division(a, b):
         result = None
     finally:
         print("Inside result: {}".format(result))
-        return result
-    
-a = 10
-b = 2
-safe_print_division(a, b)
-
-a = 5
-b = 0
-safe_print_division(a, b)
+    return result
