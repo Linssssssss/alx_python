6d784c6ec8dbfb552addb06f61c14b3248eaa9ae
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-31 13:03:39 +0300

    Best Score

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -2,13 +2,11 @@ def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
 
-
 def print_sorted_dictionary(my_dict):
     """ Print sorted dictionary """
     keys = sorted(my_dict.keys())
     for k in keys:
         print("{}: {}".format(k, my_dict[k]))
-
 
 a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,21 @@
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    best_key = None
+    max_score = float('-inf')
+
+    for key, value in a_dictionary.items():
+        if value > max_score:
+            max_score = value
+            best_key = key
+
+    return best_key
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
