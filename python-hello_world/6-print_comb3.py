374b5a52b565e17fb3b0661d64fa2847b25e32b9
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-31 15:00:12 +0300

    Inventing is combination of brains and materials. The more brains you use the less material you need

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,4 @@
-for i in range(47):
-    print("{:02d}, ".format(i), end="")
-print("89")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}, ".format(i, j), end="")
+print()
