681976b8095084ad62b96a0bbae4e0b5150a7aa7
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-31 15:31:01 +0300

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,13 +2,21 @@
 a = 1
 b = 2
 
-# Import the add function from add_0.py
-from add_0 import add
+# Define the add function
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
 # Calculate the result using the add function
 result = add(a, b)
 
 # Print the result using string formatting
 print(f"{a} + {b} = {result}")
-
-    
