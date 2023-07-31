e516dcdb813e09c85404de4d9d4302a32d8f79db
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-07-31 12:20:40 +0300

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,14 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_row = [num ** 2 for num in row]
         new_matrix.append(new_row)
-        return new_matrix
+    return new_matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
