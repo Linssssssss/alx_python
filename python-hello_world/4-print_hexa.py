6f641931be6a9070d4b46186ba5728da770b20ee
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-01 08:42:55 +0300

    Hexadecimal printing

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,2 +1,5 @@
-for i in range(99):
-    print("{:d} = 0x{:2x}".format(i, i))
+#!/usr/bin/env python3
+
+# Loop through numbers from 0 to 98 (inclusive) and directly print them with hexadecimal representation
+for num in range(99):
+    print(f"{num} = 0x{num:x}", end="")
