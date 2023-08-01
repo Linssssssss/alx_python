1ef44b90eb8f25dbde5d08854ab6a0cd74fcb80b
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-01 08:33:33 +0300

    Hexadecimal printing

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+# Initialize an empty string to store the output
+output = ""
+
 # Loop through numbers from 0 to 98 (inclusive)
 for num in range(99):
-    # Print the number in decimal and hexadecimal format
-    print(f"{num} = 0x{num:x}")
+    # Append the number in decimal and hexadecimal format to the output string
+    output += f"{num} = 0x{num:x}\n"
+
+# Print the output string
+print(output, end="")
