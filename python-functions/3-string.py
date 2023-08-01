bae2fd5c55d0eed5c21a48af915cd636c5fd1150
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-01 15:11:26 +0300

    String Manipulation Function

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python3
 def reverse_string(string):
     return string[::-1]
-
-
-print(reverse_string("Hello"))
-print(reverse_string(""))
-print(reverse_string("madam"))
-print(reverse_string("Hello, World!"))
