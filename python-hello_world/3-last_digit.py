c5943dcc2cbb19a08219a89ba411e4ae081f56e8
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-01 08:16:20 +0300

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,8 +4,10 @@ import random
 # Assign a random signed number to the variable 'number'
 number = random.randint(-10000, 10000)
 
-# Get the last digit of the number
+# Get the last digit of the number (taking into account negative numbers)
 last_digit = abs(number) % 10
+if number < 0:
+    last_digit *= -1
 
 # Print the result based on the last digit
 print(f"Last digit of {number} is {last_digit}", end=' ')
