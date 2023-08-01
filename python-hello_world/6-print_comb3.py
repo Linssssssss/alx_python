a1777de6160016127ea6ebca2502f038f886c36e
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-01 08:50:37 +0300

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,9 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}, ".format(i, j), end="")
-print()
+        # Check if it's the last combination
+        if i == 8 and j == 9:
+            # If it's the last combination, print without comma and space
+            print("{:d}{:d}".format(i, j))
+        else:
+            # For other combinations, print with comma and space
+            print("{:d}{:d}, ".format(i, j), end="")
