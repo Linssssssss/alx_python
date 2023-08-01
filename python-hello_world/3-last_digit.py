43a5c0d160a7c431d7874ee474563cf4246bc266
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-01 08:14:34 +0300

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,8 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import random
+
+# Assign a random signed number to the variable 'number'
 number = random.randint(-10000, 10000)
+
+# Get the last digit of the number
 last_digit = abs(number) % 10
-print(f"Last digit of {number} is {last_digit}", end=" ")
+
+# Print the result based on the last digit
+print(f"Last digit of {number} is {last_digit}", end=' ')
+
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
