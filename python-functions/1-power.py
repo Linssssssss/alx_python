5848eafe0109499aabb26a6919bafe976437061f
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-02 10:04:03 +0300

    a ^ b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,10 +1,15 @@
+import decimal
+
 def pow(a, b):
-    result = 1
+    decimal.getcontext().prec = 30  # Set the precision to a sufficient number of decimal places
+    a_decimal = decimal.Decimal(str(a))
+    result = decimal.Decimal('1')
+    
     if b >= 0:
         for _ in range(b):
-            result *= a
+            result *= a_decimal
     else:
         for _ in range(abs(b)):
-            result /= a
+            result /= a_decimal
 
-    return result
+    return float(result)
