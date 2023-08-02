4a90a42d84e74bc138aa46a1a612c378c20e3e24
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-02 11:22:48 +0300

    Present in both

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,6 +1,4 @@
 def common_elements(set_1, set_2):
-    return set_1.intersection(set_2)
-set_1 = {"Python", "C", "Javascript"}
-set_2 = {"Bash", "C", "Ruby", "Perl"}
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+    # Use set intersection to find the common elements between the two sets
+    common_set = set_1 & set_2
+    return common_set
