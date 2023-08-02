599bbd311a7374408716b7f2b0dcab5308949b3c
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-02 11:26:21 +0300

    Best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,21 +1,6 @@
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if not a_dictionary:
         return None
 
-    best_key = None
-    max_score = float('-inf')
-
-    for key, value in a_dictionary.items():
-        if value > max_score:
-            max_score = value
-            best_key = key
-
+    best_key = max(a_dictionary, key=a_dictionary.get)
     return best_key
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
