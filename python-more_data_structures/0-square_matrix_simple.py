bfdf71921743892a149f836580593b72982afd58
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-02 11:32:25 +0300

    Added a readme

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,12 +3,12 @@
 def square_matrix_simple(matrix=[]):
     # Implementation as shown earlier
 
- matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
 
- new_matrix = square_matrix_simple(matrix)
- print(new_matrix)
- print(matrix)
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
