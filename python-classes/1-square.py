f49e0ab72f64f07c8117fad6c34384e7dcf681eb
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-08 15:08:44 +0300

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-
-"""Class docstrings go here."""
+"""A simple Square class that defines a square by its size."""
 
 
 class Square:
-    """Class docstrings go here."""
+    """A simple Square class that defines a square by its size.
 
-    def __init__(self, size):
-        """__init__ constructor method."""
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Finds the area of the square.
+
+        Returns:
+            int : The area of the square.
+        """
+        return self.__size ** 2
