8585d4a16da54ecd21063790d2a213db29a4b1e6
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-08 19:14:03 +0300

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""
-Module contains the `BaseGeometry` class
-"""
+""" BaseGeometry class """
 
 
-class BaseGeometry():
+class BaseGeometry:
+    """A BaseGeometry class
+
+    Attributes:
+    """
     pass
