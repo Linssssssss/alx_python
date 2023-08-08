d046117168eb5387ce90fac09d0f02610c49da2a
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-08 15:07:32 +0300

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""A simple Square class that defines a square by its size."""
+
+
 class Square:
-    """Square class"""
+    """A simple Square class that defines a square by its size.
 
-    def __init__(self, size):
-        """Initialize square with size"""
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """Compute the area of the square"""
-        return self.__size * self.__size
