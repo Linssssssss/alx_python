6222d57068b09ee1cd5f1af46512fadd995aeb67
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-08 14:21:44 +0300

    size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,50 +1,14 @@
 class Square:
-    """
-    A class representing a square.
+    def __init__(self, size):
+        self._size = size
 
-    Attributes:
-        __size (int): The private attribute representing the side length of the square.
+    @property
+    def size(self):
+        return self._size
 
-    Methods:
-        __init__(self, size=0):
-            Initializes a new Square instance with the given size.
+    @size.setter
+    def size(self, size):
+        self._size = size
 
-    Usage:
-        # Creating a square instance with default size (0)
-        square_default = Square()
-        print(square_default.__dict__)  # {'_Square__size': 0}
-
-        # Creating a square instance with size 3
-        square_with_size = Square(3)
-        print(square_with_size.__dict__)  # {'_Square__size': 3}
-
-        # Attempting to create a square instance with invalid size
-        try:
-            invalid_square = Square("invalid")
-        except TypeError as e:
-            print(e)  # Output: size must be an integer
-
-        try:
-            negative_square = Square(-2)
-        except ValueError as e:
-            print(e)  # Output: size must be >= 0
-    """
-
-    def __init__(self, size=0):
-        """
-        Initializes a new Square instance with the given size.
-
-        Args:
-            size (int, optional): The side length of the square. Default is 0.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = size
+    def area(self):
+        return self.size * self.size
