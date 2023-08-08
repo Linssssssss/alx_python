320723706affd4144622d05949b64af4bdd616d6
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-08 13:08:15 +0300

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,9 @@
 class Square:
     def __init__(self, size):
-        self._size = size
+        """
+        Constructor for the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size  # Initialize the private attribute __size with the provided size
