1db4ffb24b7ef3e60924d03e1c1141fe08a2af9d
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-08 19:33:42 +0300

    Added solution for task 8

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""inherit a class from a subclass"""
+"""
+Module: 11-square.py
+"""
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """inherits from rectangle"""
+    """"Square class """
 
     def __init__(self, size):
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
-        """calculate the area of a square"""
         return self.__size * self.__size
 
     def __str__(self):
