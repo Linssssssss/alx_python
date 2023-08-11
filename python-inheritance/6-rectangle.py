f4fbe475e07fc7bd13788dfa11c363b54c21b3f4
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-11 12:15:27 +0300

    Geometery module

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,26 +1,14 @@
-class BaseGeometry:
-    def area(self):
-        """Placeholder method for calculating the area of a shape."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that a value is an integer and greater than 0."""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+#!/usr/bin/python3
+"""Define a class BaseGeometry with a public instance method"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        """Initialize a Rectangle instance with width and height."""
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    """Represents a class Rectangle that inherits from the base class"""
 
-    def __str__(self):
-        """Return a string representation of the Rectangle instance."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    def __init__(self, width, height):
+        """Initializes new rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
