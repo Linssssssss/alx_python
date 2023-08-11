366a35b725f71ac121724b2de5f2f46bbe9435eb
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-11 12:14:14 +0300

    Geometery module

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
-"""Define a class BaseGeometry with a public instance method"""
-
-
 class BaseGeometry:
-    """Represents base geometry"""
-
     def area(self):
-        """Not implemented"""
+        """Placeholder method for calculating the area of a shape."""
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that a value is an integer and greater than 0."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
