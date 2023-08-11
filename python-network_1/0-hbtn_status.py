97f86bdd18b208a05d10c9dac01e9994a74d918b
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-11 11:29:00 +0300

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status """
-from urllib.request import urlopen, Request
+"""
+ Python script
+"""
+import urllib.request
 
 
-def main():
-    """Make HTTP request to URL"""
-    req = Request('https://alx-intranet.hbtn.io/status')
-    with urlopen(req) as response:
-        content = response.read()
-
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content.decode('utf-8')}")
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+    print("Body response:\n"
+          "\t- type: {}\n"
+          "\t- content: {}\n"
+          "\t- utf8 content: {}".
+          format(type(html), html, html.decode('utf-8'))
+          )
