024513c9a84ab64ff8d4e815eac16cb90fdd91c7
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-11 11:37:33 +0300

    Response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,13 +1,15 @@
-#!/usr/bin/python3
-"""
-comment
-"""
-import urllib.request
-import sys
+#!/usr/bin/env python3
+import requests  # Import the requests module for making HTTP requests
+import sys      # Import the sys module for accessing command-line arguments
 
+# Get the URL from the command-line argument
+url = sys.argv[1]
 
-if __name__ == '__main__':
-    """request"""
-    url = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(url) as response:
-        print(response.getheader('X-Request-Id'))
+# Make an HTTP GET request to the URL
+response = requests.get(url)
+
+# Get the value of the 'X-Request-Id' header from the response
+x_request_id = response.headers.get('X-Request-Id')
+
+# Display the value of the 'X-Request-Id' header
+print(x_request_id)
