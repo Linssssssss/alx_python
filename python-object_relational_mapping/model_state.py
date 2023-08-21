dfcef0b01a7deaac83f5cac13ee5f842277fe052
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-21 09:40:07 +0300

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+'''
+contains the class definition of a State and an
+instance Base = declarative_base()
+'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+class State(Base):
+    ''' Empty class that inherits from Base '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
