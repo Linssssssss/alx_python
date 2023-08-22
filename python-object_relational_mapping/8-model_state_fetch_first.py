109d932caa1685acad87cb96fd2c3556e24a5671
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-22 08:08:48 +0300

    Update

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
-''' Prints the first State object from the database hbtn_0e_6_usa '''
-from sys import argv
+""" lists all State objects from the database hbtn_0e_6_usa
+using SQLAlchemy """
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
+
+
+def main():
+    """main function """
+
+    dialect = 'mysql'
+    driver = 'mysqldb'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    host = 'localhost'
+    port = '3306'
+    database = sys.argv[3]
+    engine = create_engine("{}+{}://{}:{}@{}:{}/{}".format(dialect,
+                                                           driver,
+                                                           username,
+                                                           password,
+                                                           host,
+                                                           port,
+                                                           database))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rs = session.query(State).order_by(State.id).first()
+    if rs is None:
+        print('Nothing')
+    else:
+        print("{}: {}".format(rs.id, rs.name))
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]))
-    InstanceSession = sessionmaker(bind=engine)
-    session = InstanceSession()
-
-    state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
-    session.close()
+    main()
--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,42 +1,38 @@
-#!/usr/bin/env python3
-"""
-Script to list all State objects that contain the letter 'a'
-from the database hbtn_0e_6_usa using SQLAlchemy.
-"""
-
+#!/usr/bin/python3
+""" lists all State objects from the database hbtn_0e_6_usa
+using SQLAlchemy """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(
-            sys.argv[0]))
-        sys.exit(1)
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+def main():
+    """main function """
 
-    # Creating an engine to connect to the MySQL server
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            mysql_username, mysql_password, database_name
-        )
-    )
-
-    # Bind the engine to the Base class
-    Base.metadata.bind = engine
-
-    # Creating a session
+    dialect = 'mysql'
+    driver = 'mysqldb'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    host = 'localhost'
+    port = '3306'
+    database = sys.argv[3]
+    engine = create_engine("{}+{}://{}:{}@{}:{}/{}".format(dialect,
+                                                           driver,
+                                                           username,
+                                                           password,
+                                                           host,
+                                                           port,
+                                                           database))
     Session = sessionmaker(bind=engine)
     session = Session()
+    rs = session.query(State).filter(State.name.like('%a%')).all()
+# result = engine.execute("SELECT * FROM states WHERE name \
+#                          LIKE '%a%' ORDER BY id")
+# rs = session.query(State).instances(result)
+    for r in rs:
+        print("{}: {}".format(r.id, r.name))
 
-    # Querying the database for State objects containing the letter 'a'
-    states_with_a = (
-        session.query(State)
-        .filter(State.name.contains("a"))
-        .order_by(State.id)
-        .all()
-    )
+
+if __name__ == "__main__":
+    main()
