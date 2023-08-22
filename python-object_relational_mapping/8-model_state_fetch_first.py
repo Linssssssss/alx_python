51794aac459e85444f1997cd76bf3d8afcbc0c73
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-22 08:30:31 +0300

    Fixed errors

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
-"""Prints the first State object from the database hbtn_0e_6_usa."""
-
+"""
+script that prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(
-            sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
 
-    # Get MySQL username, password, and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    # Create engine to connect to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database),
-                           pool_pre_ping=True)
-
-    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
+    Base.metadata.create_all(engine)
 
-    # Retrieve the first State object from the database and display it
-    first_state = session.query(State).order_by(State.id).first()
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print(f"{state.id}: {state.name}")
     else:
         print("Nothing")
-
-    # Close the session
-    session.close()
--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
-"""Lists all State objects that contain the letter a from the database hbtn_0e_6_usa."""
-
+"""
+script that lists all State objects that contain
+the letter a from the database hbtn_0e_6_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(
-            sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
 
-    # Get MySQL username, password, and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]), pool_pre_ping=True)
 
-    # Create engine to connect to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database),
-                           pool_pre_ping=True)
-
-    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
+    Base.metadata.create_all(engine)
 
-    # Retrieve and display State objects containing the letter 'a' from the database
-    states_with_a = session.query(State).filter(
+    states = session.query(State).filter(
         State.name.like('%a%')).order_by(State.id).all()
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
 
-    # Close the session
-    session.close()
+    for state in states:
+        print(f"{state.id}: {state.name}")
