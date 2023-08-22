12e424b677776c1e30a2b82fc0c5ba1650e2e237
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-22 08:16:36 +0300

    All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-"""Lists all State objects from the database hbtn_0e_6_usa."""
+#!/usr/bin/env python3
+"""Script that lists all State objects from the database hbtn_0e_6_usa"""
 
 import sys
 from sqlalchemy import create_engine
@@ -7,29 +7,31 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
     if len(sys.argv) != 4:
         print("Usage: {} <mysql username> <mysql password> <database name>".format(
             sys.argv[0]))
         sys.exit(1)
 
-    # Get MySQL username, password, and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    # Create engine to connect to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database),
-                           pool_pre_ping=True)
+    # Create an engine to connect to the database
+    engine = create_engine(
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}',
+        pool_pre_ping=True
+    )
 
-    # Create a session to interact with the database
+    # Bind the engine to the Base class
+    Base.metadata.create_all(engine)
+
+    # Create a Session class bound to the engine
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Retrieve and display State objects from the database, sorted by states.id
+    # Query all State objects from the database and sort by states.id
     states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
 
-    # Close the session
-    session.close()
+    # Print the results
+    for state in states:
+        print(f"{state.id}: {state.name}")
