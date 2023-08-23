26b2169bbe53d194f9854e7fc649aed64b73e683
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-23 09:49:26 +0300

    All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,24 +1,32 @@
-#!/usr/bin/python3
-"""
-script that lists all State objects from the database hbtn_0e_6_usa
-"""
+#!/usr/bin/env python3
+"""Script to list all State objects from a MySQL database using SQLAlchemy"""
+
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(
+            sys.argv[0]))
+        sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]), pool_pre_ping=True)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
+    # Create an engine to connect to the MySQL server
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        mysql_username, mysql_password, database_name))
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
 
+    # Query and print State objects in ascending order by states.id
     states = session.query(State).order_by(State.id).all()
-
     for state in states:
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
