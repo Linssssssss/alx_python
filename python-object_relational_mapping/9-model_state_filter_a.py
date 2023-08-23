fe595fde5c71575d65188403fd8820d63c79e2b8
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-23 10:33:26 +0300

    Contains a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,26 +1,33 @@
-#!/usr/bin/python3
-"""
-script that lists all State objects that contain
-the letter a from the database hbtn_0e_6_usa
-"""
+#!/usr/bin/env python3
+"""Script to list all State objects containing the letter "a" from a MySQL database using SQLAlchemy"""
+
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(
+            sys.argv[0]))
+        sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]), pool_pre_ping=True)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
+    # Create an engine to connect to the MySQL server
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        mysql_username, mysql_password, database_name))
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
 
-    states = session.query(State).filter(
+    # Query and print State objects containing the letter "a" in ascending order by states.id
+    states_with_a = session.query(State).filter(
         State.name.like('%a%')).order_by(State.id).all()
+    for state in states_with_a:
+        print("{}: {}".format(state.id, state.name))
 
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    session.close()
