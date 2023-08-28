6ad05f375147f4357613668d4a41cd24919e2b43
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-28 10:45:02 +0300

    Number Template

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,44 +1,40 @@
-#!/usr/bin/python3
-'''
-    starts a Flask web application:
-'''
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    return ("Hello HBNB!")
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return ("HBNB")
+    return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_is_fun(text):
-    return ("C {}".format(text.replace("_", " ")))
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_is_cool(text="is cool"):
-    return ("Python {}".format(text.replace("_", " ")))
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    return "{} is a number".format(n)
+    return f"{n} is a number"
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template("5-number.html", number=n)
+    return render_template('5-number_template.html', n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
