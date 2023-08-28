fbcb2510c1fb6bbe1aa9721d5073caaaf4bc2ff4
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-28 10:48:45 +0300

    Number Template

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,40 +1,48 @@
-from flask import Flask, render_template
-
+#!/usr/bin/python3
+'''
+    starts a Flask web application:
+'''
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route("/", strict_slashes=False)
+def hello():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    text = text.replace('_', ' ')
-    return f"C {text}"
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    text = text.replace('_', ' ')
-    return f"Python {text}"
+@app.route("/python/(<text>)", strict_slashes=False)
+def python(text):
+    text = text or "is cool"
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route("/number/<n>", strict_slashes=False)
 def number(n):
-    return f"{n} is a number"
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+    else:
+        return "404 Not Found"
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route("/number_template/<n>", strict_slashes=False)
 def number_template(n):
-    return render_template('5-number_template.html', n=n)
+    if isinstance(n, int):
+        return render_template("number.html", n=n)
+    else:
+        return "404 Not Found"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
