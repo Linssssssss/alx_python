cc0cc47271c1afac4be266b5f599083362e239df
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-28 10:54:21 +0300

    Number Template

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -2,49 +2,43 @@
 '''
     starts a Flask web application:
 '''
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
-    return "Hello HBNB!"
+    return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    return ("HBNB")
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+def c_is_fun(text):
+    return ("C {}".format(text.replace("_", " ")))
 
 
-@app.route("/python/(<text>)", strict_slashes=False)
-def python(text):
-    text = text or "is cool"
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    return ("Python {}".format(text.replace("_", " ")))
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    if isinstance(n, int):
-        return "{} is a number".format(n)
-    else:
-        return "404 Not Found"
+    return "{} is a number".format(n)
 
 
-@app.route("/number_template/<n>", strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    if isinstance(n, int):
-        return render_template("number.html", n=n)
-    else:
-        return "404 Not Found"
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
