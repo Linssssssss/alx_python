b1b70b963bf76a0c331e35162c89358f89915683
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-28 10:28:13 +0300

    Python is cool

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''
-starts a Flask web application:
+    starts a Flask web application:
 '''
 from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -20,6 +21,12 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    return ("Python {}".format(text.replace("_", " ")))
 
 
 if __name__ == "__main__":
