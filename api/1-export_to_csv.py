b2a10bdedae458b7ee543ba5e98f43e7c8aa76cf
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-10-02 11:38:11 +0300

    Export to CSV

--- api/1-export_to_csv.py
+++ api/1-export_to_csv.py
@@ -43,7 +43,7 @@ def export_employee_todo_to_csv(employee_id):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 export_to_CSV.py <employee_id>")
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
