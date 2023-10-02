58a4b815ddc635ad8386d2cda3bf5033335f1e63
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-10-02 11:30:30 +0300

    Export to CSV

--- api/1-export_to_csv.py
+++ api/1-export_to_csv.py
@@ -1,14 +1,15 @@
 import requests
 import csv
+import sys
 
 
-def get_employee_todo_progress(employee_id):
+def export_employee_todo_to_csv(employee_id):
     # Define the base URL for the JSONPlaceholder API
     base_url = "https://jsonplaceholder.typicode.com"
 
     # Construct the URLs for employee details and TODO items
     employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/users/{employee_id}/todos"
+    todo_url = f"{base_url}/todos?userId={employee_id}"
 
     try:
         # Fetch employee details
@@ -23,17 +24,17 @@ def get_employee_todo_progress(employee_id):
         filename = f"{employee_id}.csv"
         with open(filename, mode='w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
+
             # Write the CSV header
             csv_writer.writerow(
                 ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
             # Write each task to the CSV file
             for task in todo_data:
-                task_completed_status = "Completed" if task['completed'] else "Not Completed"
+                task_completed_status = "True" if task['completed'] else "False"
                 csv_writer.writerow(
                     [employee_id, employee_data['username'], task_completed_status, task['title']])
 
-        # Print a message to indicate that the CSV file has been created
         print(f"CSV file '{filename}' created successfully.")
 
     except requests.exceptions.RequestException as e:
@@ -41,5 +42,9 @@ def get_employee_todo_progress(employee_id):
 
 
 if __name__ == "__main__":
-    employee_id = int(input("Enter the employee ID: "))
-    get_employee_todo_progress(employee_id)
+    if len(sys.argv) != 2:
+        print("Usage: python3 export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    export_employee_todo_to_csv(employee_id)
