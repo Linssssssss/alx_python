a400e0dc8a6f80ff9a56d79a05e7070a2ea62efb
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-10-02 12:33:12 +0300

    Export to CSV

--- api/1-export_to_csv.py
+++ api/1-export_to_csv.py
@@ -1,50 +1,63 @@
 import requests
-import csv
 import sys
+import csv
 
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python3 export_to_CSV.py <employee_id>")
+    sys.exit(1)
 
-def export_employee_todo_to_csv(employee_id):
-    # Define the base URL for the JSONPlaceholder API
-    base_url = "https://jsonplaceholder.typicode.com"
+# Get the employee ID from the command-line argument
+employee_id = int(sys.argv[1])
 
-    # Construct the URLs for employee details and TODO items
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
+# Define the base URL for the JSONPlaceholder API
+base_url = "https://jsonplaceholder.typicode.com"
 
-    try:
-        # Fetch employee details
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
+# Create URLs for getting employee details and TODO list
+employee_url = f"{base_url}/users/{employee_id}"
+todo_url = f"{base_url}/users/{employee_id}/todos"
 
-        # Fetch TODO items for the employee
-        todo_response = requests.get(todo_url)
-        todo_data = todo_response.json()
+# Send GET requests to the API
+employee_response = requests.get(employee_url)
+todo_response = requests.get(todo_url)
 
-        # Create a CSV file for the employee
-        filename = f"{employee_id}.csv"
-        with open(filename, mode='w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
+# Check if the responses are successful
+if employee_response.status_code != 200:
+    print(
+        f"Error: Unable to retrieve employee details (Status Code: {employee_response.status_code})")
+    sys.exit(1)
 
-            # Write the CSV header
-            csv_writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+if todo_response.status_code != 200:
+    print(
+        f"Error: Unable to retrieve TODO list (Status Code: {todo_response.status_code})")
+    sys.exit(1)
 
-            # Write each task to the CSV file
-            for task in todo_data:
-                task_completed_status = "True" if task['completed'] else "False"
-                csv_writer.writerow(
-                    [employee_id, employee_data['username'], task_completed_status, task['title']])
+# Parse JSON responses
+employee_data = employee_response.json()
+todo_data = todo_response.json()
 
-        print(f"CSV file '{filename}' created successfully.")
+# Extract employee name and username
+employee_name = employee_data['name']
+employee_username = employee_data['username']
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+# Create a CSV file with the user ID as the filename
+csv_filename = f"{employee_id}.csv"
 
+# Open the CSV file for writing
+with open(csv_filename, mode='w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
+    # Write the CSV header
+    csv_writer.writerow(
+        ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-    employee_id = int(sys.argv[1])
-    export_employee_todo_to_csv(employee_id)
+    # Write TODO list data to the CSV file
+    for task in todo_data:
+        task_completed_status = str(task['completed'])
+        task_title = task['title']
+        csv_writer.writerow([employee_id, employee_username,
+                            task_completed_status, task_title])
+
+# Print a message indicating the CSV file has been created
+print(f"Data exported to {csv_filename}")
